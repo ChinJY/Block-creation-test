1a739ab1089ce9c51a5ff45143ebeadcd09fff6a
Immortalin <immortalin@icloud.com>
2016-01-07 20:58:54 +0800

    Add five is working. Jiayi do you take sadistic pleasure in wasting time, either your own or other people's?

--- blockly/demos/code/javascript.js
+++ blockly/demos/code/javascript.js
@@ -71,7 +71,7 @@ Blockly.JavaScript['page_text'] = function(block) {
 Blockly.JavaScript['add_five'] = function(block) {
   var text_number = block.getFieldValue('number');
   // TODO: Assemble JavaScript into code variable.
-  var code = String(parseInt(text_number) + 5 + ";")
+  var code = String(parseInt(text_number) + "+" + "5")
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_ADDITION];
 };
